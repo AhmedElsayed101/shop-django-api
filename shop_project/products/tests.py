973822d690fee1e626932160dcc895e4b1dc20bc
AhmedElsayed101 <ahmedessayed101@gmail.com>
2022-01-15 22:49:24 +0200

    Create tests for product

--- shop_project/products/tests.py
+++ shop_project/products/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+from .models import Product
+
+class BookTests(TestCase):
+
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username = 'test',
+            email = 'test@email.com',
+            password = 'test'
+        )
+
+        self.product = Product.objects.create(
+            name='product1',
+            price=10,
+            seller = self.user
+        )
+
+    
+    def test_name_product(self):
+        
+        product = Product.objects.get(id=1)
+        expected_name = product.name
+        self.assertEquals(expected_name, 'product1')
+
+    
+    def test_name_product(self):
+        
+        product = Product.objects.get(id=1)
+        expected_price = product.price
+        self.assertEquals(expected_price, 10)
+
+    
