54d646d2ce04b872576f79fb53b6667ffb39a5ce
Ahmed ELSAYED <aes@d-fi.fr>
2022-01-15 15:26:55 +0200

    Register model product to admin panel

--- shop_project/products/admin.py
+++ shop_project/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['seller', 'name', 'price',]
+    list_filter = ['seller', 'name', 'price',]
+    raw_id_fields = ('seller',)
