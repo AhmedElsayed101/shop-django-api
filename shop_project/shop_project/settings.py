f504ab78b8cff99650e6323020f40f7070485284
Ahmed ELSAYED <aes@d-fi.fr>
2022-01-15 15:37:19 +0200

    user corsheaders app

--- shop_project/shop_project/settings.py
+++ shop_project/shop_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     #3rd
     'rest_framework',
+    "corsheaders",
+
     # 'rest_framework.authtoken',
 
     #local
@@ -50,6 +52,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
