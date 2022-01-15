a9108c2a124177bb335967a30a3fdaa8379d95f1
Ahmed ELSAYED <aes@d-fi.fr>
2022-01-15 15:29:39 +0200

    Create product list and detail views

--- shop_project/products/views.py
+++ shop_project/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Product
+from .serializers import Product, ProductSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class ProductList(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
