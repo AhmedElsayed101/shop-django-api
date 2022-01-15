56e17e4cd5700be1ceb4911deff81e1f293833aa
Ahmed ELSAYED <aes@d-fi.fr>
2022-01-15 15:01:19 +0200

    Create product model

--- shop_project/products/models.py
+++ shop_project/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    seller = models.ForeignKey(User,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('price',)
+    def __str__(self):
+        return self.name
